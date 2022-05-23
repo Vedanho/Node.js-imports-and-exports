4dd92224920076361251d39953b54cf65fb8345d
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-05-23 12:41:34 +0300

    Made the tasks about export and import in node.js

---
+++ variables/first.js
@@ -0,0 +1,5 @@
+export const number1 = 3;
+export const number2 = 2;
+export function mult (x, y) {
+    return x * y
+}
---
+++ variables/second.js
@@ -0,0 +1,2 @@
+import { number1, number2, mult } from "./first.js";
+console.log(number1)
